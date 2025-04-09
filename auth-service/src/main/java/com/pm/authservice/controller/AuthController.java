46aecb21f3ac1963df9e77969cb938f463bb2735
CompCodeHub <Suyashtalekar2000@gmail.com>
2025-04-08 20:54:54 -0400

    Implemented route for auth-service api docs

--- auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
+++ auth-service/src/main/java/com/pm/authservice/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.dto.LoginResponseDTO;
 import com.pm.authservice.service.AuthService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@Tag(name = "Auth", description = "API for managing auth")
 public class AuthController {
 
     private final AuthService authService;
